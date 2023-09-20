ae1c3f463c8267c9f82a703c7ef45b3f1cf8af08
Aline Guimaraes <aline.tguimaraes@gmail.com>
2023-09-20 18:59:37 -0300

    finalização para github

---
+++ main.py
@@ -0,0 +1,38 @@
+#
+# Autora: Aline Torres Guimarães
+# Jogo: Adivinhe uma fruta
+# 
+
+import random
+
+
+print("----- Adivinhe uma fruta <3 -----")
+
+nome = input("Qual o seu nome? ")
+
+print("Olá, " + nome + "! Boa sorte!")
+print("Você tem 5 chances")
+
+frutas = ["maçã", "uva", "melancia", "melão", "limão", "laranja", "banana", "abacaxi", "cajá", "kiwi", "manga", "morango", "amora", "pera", "carambola", "ameixa", "abacate"] 
+
+
+def jogo():
+    fruta_secreta = random.choice (frutas)
+    palpite = 0
+    chance = 1
+    
+
+    while palpite != fruta_secreta:
+        palpite=input("Digite o nome de uma fruta: ")
+        palpite.lower()
+        if (palpite == fruta_secreta):
+            print("Parabéns, você acertou! <3")
+        else:
+            print("Ah, não! Tente novamente. :(")
+            print("Tentativa nº " + str(chance))
+            chance = chance + 1
+            if chance > 5:
+                print("Você perdeu :( Tente outra vez!")
+                print("A fruta secreta era: " + fruta_secreta + " <3")
+                break
+jogo()
